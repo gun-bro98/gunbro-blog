7276ae9abeaa9c32248655b2a887e549b1333b7e
gunhyung <one0374@naver.com>
2024-02-11 22:55:35 +0900

    next config 수정

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
