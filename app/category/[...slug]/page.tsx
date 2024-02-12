d402710aca2abb3a4f5185e4a983440db07fb374
gunhyung <one0374@naver.com>
2024-02-12 09:53:42 +0900

    카테고리 페이지 오류 찾는 중

--- app/category/[...slug]/page.tsx
+++ app/category/[...slug]/page.tsx
@@ -4,12 +4,9 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 export const generateStaticParams = async () =>
-  allPosts.map((post) => {
-    console.info("[1]",post);
-    return ({
-      slug: post._raw.flattenedPath.split("/").slice(1),
-    })
-  });
+  allPosts.map((post) => ({
+    slug: post._raw.flattenedPath.split("/").slice(1),
+  }));
 
 export const generateMetadata = ({
   params,
@@ -20,11 +17,10 @@ export const generateMetadata = ({
 };
 
 function Category({ params }: { params: { slug: string[] } }) {
-  const postList = allPosts.filter((post) => {
-    console.info("[2]", post);
-    return post._raw.flattenedPath.includes(params.slug.join("/"));
-  });
-  if (postList.length === 0) return notFound();
+  // const postList = allPosts.filter((post) => {
+  //   return post._raw.flattenedPath.includes(params.slug.join("/"));
+  // });
+  // if (postList.length === 0) return notFound();
   return (
     <section className="pt-8 w-full">
       <h2 className="text-normal text-sm font-bold">
@@ -32,9 +28,9 @@ function Category({ params }: { params: { slug: string[] } }) {
       </h2>
       <div className="border-t pt-5 w-full mt-5 border-t-[#00000050]" />
       <div className="tablet:flex-col tablet:gap-[20px] flex gap-[2%] flex-wrap">
-        {postList.map((post, key) => (
+        {/* {postList.map((post, key) => (
           <PreviewCard key={key} post={post} />
-        ))}
+        ))} */}
       </div>
     </section>
   );
