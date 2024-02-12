81949f2078160299c11f706362aae8d261749d02
gunhyung <one0374@naver.com>
2024-02-12 14:20:44 +0900

    카테고리 페이지 오류 찾는 중

--- next.config.js
+++ next.config.js
@@ -5,4 +5,21 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withContentlayer((phase) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    const sourceDir = path.join(__dirname, 'content', 'posts');
+    const destDir = path.join(__dirname, '.next', 'static', 'content', 'posts');
+
+    if (!existsSync(destDir)) {
+      mkdirSync(destDir, { recursive: true });
+    }
+
+    readdirSync(sourceDir).forEach((file) => {
+      const srcFile = path.join(sourceDir, file);
+      const destFile = path.join(destDir, file);
+      copyFileSync(srcFile, destFile);
+    });
+  }
+
+  return nextConfig
+});
