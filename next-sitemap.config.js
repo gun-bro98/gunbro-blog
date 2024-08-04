20e27c057b691471ba6a7d5324b3819e0feb9966
gunhyung <one0374@naver.com>
2024-08-04 11:06:58 +0900

    next-sitemap.config.js 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://blog.gunbro.kr', // 사이트의 기본 URL
-  generateRobotsTxt: true, // robots.txt 생성 여부
-  changefreq: 'daily', // 기본 changefreq 값
-  priority: 0.7, // 기본 priority 값
-  sitemapSize: 5000, // 각 sitemap 파일의 최대 URL 수
-  exclude: ['/404', '/500'], // 사이트맵에서 제외할 페이지
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-    ],
+  siteUrl: 'https://blog.gunbro.kr',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/404', '/500'],
+  // 페이지 필터링을 통해 불필요한 태그를 제거하는 방법
+  transform: async (config, path) => {
+    return {
+      loc: path, // 현재 경로
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ? config.alternateRefs : [],
+    };
   },
-  additionalSitemaps: [
-    'https://blog.gunbro.kr/sitemap-0.xml', // 추가 사이트맵 URL
-  ],
 };
